ed3ba0da16c904db8bc76c67631e593b731b490d
Harish Naik <hnaik@pro.local>
2020-03-18 21:39:33 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# iconn - Interpretability in COnvolutional Neural Networks
+
+This is an exploration of interpretability in Convolutional Neural Networks
---
+++ apps/iconn.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+from argparse import ArgumentParser
+
+
+def main(args):
+    ...
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(sys.argv)
+
+    main(parser.parse_args())
