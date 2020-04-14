4e682aa2aa90d535af666bf44623baa7f31209e9
Harish Naik <harishgnaik@gmail.com>
2020-04-13 19:24:49 -0500

    Logging Configuration

--- iconn/utils.py
+++ iconn/utils.py
@@ -4,8 +4,27 @@ import sys
 
 from pathlib import Path
 
+logger = logging.getLogger(__name__)
+
+log_levels = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+    'fatal': logging.FATAL,
+}
+
 
 def init_logging():
     self_path = Path(sys.argv[0])
     root_dir = self_path.parent.parent.absolute()
     logging.config.fileConfig(root_dir / 'logging_conf.ini')
+
+
+def get_log_levels():
+    return log_levels.keys()
+
+
+def set_level(logger_instance, level_name):
+    return logger_instance.setLevel(log_levels[level_name])
